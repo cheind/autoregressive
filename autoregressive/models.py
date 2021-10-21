e04b21579a43d98e29814658083a4668dc1088bf
Christoph.Heindl <cheind@profactor.at>
2021-10-21 15:12:44 +0200

    refactored for a simpler interface

--- autoregressive/models.py
+++ autoregressive/models.py
@@ -13,180 +13,86 @@ _logger = logging.getLogger("pytorch_lightning")
 _logger.setLevel(logging.INFO)
 
 
-def _exp_weights(T, n):
-    """Returns exponential decaying weights for T terms from 1.0 down to n (n>0)"""
-    n = torch.as_tensor(n).float()
-    x = torch.arange(0, T, 1)
-    b = -torch.log(n) / (T - 1)
-    return torch.exp(-x * b)
+def fit(
+    model: pl.LightningModule,
+    dataset_train,
+    dataset_val,
+    batch_size: int = 64,
+    max_epochs: int = 30,
+    num_train_curves: int = 2 ** 11,
+    num_val_curves: int = 2 ** 9,
+):
+    from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+
+    is_train_iterable = isinstance(dataset_train, torch.utils.data.IterableDataset)
+    is_val_iterable = isinstance(dataset_val, torch.utils.data.IterableDataset)
+
+    train_loader = data.DataLoader(dataset_train, batch_size, num_workers=4)
+    val_loader = data.DataLoader(dataset_val, batch_size, num_workers=4)
+
+    ckpt = ModelCheckpoint(
+        monitor="val_loss",
+        filename="regressionwavenet-{epoch:02d}-{val_loss:.4f}",
+    )
+    lrm = LearningRateMonitor(logging_interval="step")
+
+    trainer_args = {
+        "gpus": 1,
+        "callbacks": [ckpt, lrm],
+        "max_epochs": max_epochs,
+    }
+    if is_train_iterable:
+        trainer_args.update({"limit_train_batches": num_train_curves // batch_size}),
+    if is_val_iterable:
+        trainer_args.update({"limit_val_batches": num_val_curves // batch_size}),
+
+    trainer = pl.Trainer(**trainer_args)
+    trainer.fit(model, train_dataloader=train_loader, val_dataloaders=val_loader)
+    _logger.info(f"Best model on validation {ckpt.best_model_path}")
 
 
-class RegressionWaveNet(pl.LightningModule):
-    def __init__(
-        self,
-        in_channels: int = 1,
-        forecast_steps: int = 64,
-        residual_channels: int = 32,
-        skip_channels: int = 32,
-        num_blocks: int = 1,
-        num_layers: int = 7,
-        loss_require_full_receptive_field: bool = True,
-        loss_exp_decay_weights: bool = False,
-    ) -> None:
-        super().__init__()
-        self.wave = wave.WaveNetLinear(
-            in_channels,
-            forecast_steps,
-            residual_channels,
-            skip_channels,
-            num_blocks,
-            num_layers,
-        )
-        self.forecast_steps = forecast_steps
-        self.loss_require_full_receptive_field = loss_require_full_receptive_field
-        self.receptive_field = self.wave.receptive_field
-        self.use_positional_encoding = in_channels > 1
-        self.loss_exp_decay_weights = loss_exp_decay_weights
-        if self.loss_exp_decay_weights and self.forecast_steps > 1:
-            self.register_buffer(
-                "l1_weights", _exp_weights(self.forecast_steps, 1e-3).view(1, -1, 1)
-            )
-        else:
-            self.l1_weights = 1.0
-        _logger.info(f"Receptive field of model {self.receptive_field}")
-        super().save_hyperparameters()
+# class NStepPrediction:
+#     """Predict n-steps using direct model output"""
 
-    def forward(self, x, return_outputs: bool = False):
-        return self.wave(x, return_outputs=return_outputs)
+#     def __init__(self, model) -> None:
+#         self.model = model
+#         self.model.eval()
 
-    def forward_one(self, x, queues):
-        return self.wave.forward_one(x, queues)
-
-    def configure_optimizers(self):
-        import torch.optim.lr_scheduler as sched
-
-        opt = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return {
-            "optimizer": opt,
-            "lr_scheduler": {
-                "scheduler": sched.ReduceLROnPlateau(
-                    opt, mode="min", factor=0.5, patience=1, min_lr=1e-7, threshold=1e-7
-                ),
-                "monitor": "val_loss",
-                "interval": "epoch",
-                "frequency": 1,
-            },
-        }
-
-    def training_step(self, batch, batch_idx):
-        loss = self._step(batch, batch_idx)
-        self.log("train_loss", loss)
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        loss = self._step(batch, batch_idx)
-        self.log("val_loss", loss, prog_bar=True)
-        return loss
-
-    def _step(self, batch, batch_idx) -> torch.FloatTensor:
-        x = batch["x"][..., :-1]
-        t = batch["t"][..., :-1]
-        y = batch["xo"][..., 1:]
-        y = y.unfold(-1, self.forecast_steps, 1).permute(0, 2, 1)
-        x = x.unsqueeze(1)
-        t = t.unsqueeze(1)
-        if self.use_positional_encoding:
-            y_hat = self(torch.cat((x, t), 1))
-        else:
-            y_hat = self(x)
-        n = y.shape[-1]
-        r = self.receptive_field if self.loss_require_full_receptive_field else 0
-        losses = F.l1_loss(y_hat[..., r:n], y[..., r:], reduction="none")
-        return torch.mean(losses * self.l1_weights)
-
-    def fit(
-        self,
-        dataset_train,
-        dataset_val,
-        batch_size: int = 64,
-        max_epochs: int = 30,
-        num_train_curves: int = 2 ** 11,
-        num_val_curves: int = 2 ** 9,
-    ):
-        from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
-        from torch.utils.data import IterableDataset
-
-        is_train_iterable = isinstance(dataset_train, torch.utils.data.IterableDataset)
-        is_val_iterable = isinstance(dataset_val, torch.utils.data.IterableDataset)
-
-        train_loader = data.DataLoader(dataset_train, batch_size, num_workers=4)
-        val_loader = data.DataLoader(dataset_val, batch_size, num_workers=4)
-
-        ckpt = ModelCheckpoint(
-            monitor="val_loss",
-            filename="regressionwavenet-{epoch:02d}-{val_loss:.4f}",
-        )
-        lrm = LearningRateMonitor(logging_interval="step")
-
-        trainer_args = {
-            "gpus": 1,
-            "callbacks": [ckpt, lrm],
-            "max_epochs": max_epochs,
-        }
-        if is_train_iterable:
-            trainer_args.update(
-                {"limit_train_batches": num_train_curves // batch_size}
-            ),
-        if is_val_iterable:
-            trainer_args.update({"limit_val_batches": num_val_curves // batch_size}),
-
-        trainer = pl.Trainer(**trainer_args)
-        trainer.fit(self, train_dataloader=train_loader, val_dataloaders=val_loader)
-        _logger.info(f"Best model on validation {ckpt.best_model_path}")
+#     @torch.no_grad()
+#     def predict(self, x: torch.Tensor, t: torch.Tensor, horizon: int) -> torch.Tensor:
+#         assert horizon <= self.model.forecast_steps
+#         x = x.view((1, 1, -1))
+#         t = t.view((1, 1, -1))
+#         if self.model.use_positional_encoding:
+#             y = self.model(torch.cat((x, t), 1))
+#         else:
+#             y = self.model(x)
+#         return y[0, :horizon, -1]
 
 
-class NStepPrediction:
-    """Predict n-steps using direct model output"""
+# class NaiveGeneration:
+#     def __init__(self, model) -> None:
+#         self.model = model
+#         self.model.eval()
+#         assert not self.model.use_positional_encoding
 
-    def __init__(self, model) -> None:
-        self.model = model
-        self.model.eval()
+#     @torch.no_grad()
+#     def predict(self, x: torch.Tensor, t: torch.Tensor, horizon: int) -> torch.Tensor:
+#         import time
 
-    @torch.no_grad()
-    def predict(self, x: torch.Tensor, t: torch.Tensor, horizon: int) -> torch.Tensor:
-        assert horizon <= self.model.forecast_steps
-        x = x.view((1, 1, -1))
-        t = t.view((1, 1, -1))
-        if self.model.use_positional_encoding:
-            y = self.model(torch.cat((x, t), 1))
-        else:
-            y = self.model(x)
-        return y[0, :horizon, -1]
-
-
-class NaiveGeneration:
-    def __init__(self, model) -> None:
-        self.model = model
-        self.model.eval()
-        assert not self.model.use_positional_encoding
-
-    @torch.no_grad()
-    def predict(self, x: torch.Tensor, t: torch.Tensor, horizon: int) -> torch.Tensor:
-        import time
-
-        t0 = time.time()
-        r = self.model.receptive_field
-        y = x.new_empty(r + horizon)
-        y[:r] = x[-r:]  # Copy last `see` observations
-        for h in range(horizon):
-            p = self.model(y[h : h + r].view(1, 1, -1))
-            y[h + r] = p[0, 0, -1]
-        print("naive took", (time.time() - t0))
-        return y[r:]
+#         t0 = time.time()
+#         r = self.model.receptive_field
+#         y = x.new_empty(r + horizon)
+#         y[:r] = x[-r:]  # Copy last `see` observations
+#         for h in range(horizon):
+#             p = self.model(y[h : h + r].view(1, 1, -1))
+#             y[h + r] = p[0, 0, -1]
+#         print("naive took", (time.time() - t0))
+#         return y[r:]
 
 
 class FastGeneration:
-    def __init__(self, model: RegressionWaveNet) -> None:
+    def __init__(self, model: wave.RegressionWaveNet) -> None:
         self.model = model
         self.model.eval()
 
@@ -207,9 +113,8 @@ class FastGeneration:
         # queues need to contain states (x_(t-r), x_(t-r+1),...,x_(t-1))
 
         y[:r] = x[-r:]  # Copy last `see` observations
-        _, outputs = self.model(y[: r - 1].view(1, 1, -1), return_outputs=True)
-        queues = wave.create_fast_queues(self.model.wave.features, outputs)
-        print(queues[2][0, :10, -1])
+        _, layer_inputs, _ = self.model.encode(y[: r - 1].view(1, 1, -1))
+        queues = self.model.create_fast_queues(layer_inputs)
 
         # queues = wave.create_fast_queues(
         #     self.model.wave.features, outputs=None, device=x.device
@@ -231,17 +136,17 @@ def train(args):
     logging.basicConfig(level=logging.INFO)
 
     dataset_train, dataset_val = dataset.create_default_datasets()
-    model = RegressionWaveNet(
+    model = wave.LitRegressionWaveNet(
         in_channels=1,
         forecast_steps=1,
         residual_channels=128,
         skip_channels=128,
         num_blocks=1,
-        num_layers=9,
-        loss_require_full_receptive_field=True,
-        loss_exp_decay_weights=True,
+        num_layers_per_block=9,
+        train_full_receptive_field=True,
+        train_exp_decay=True,
     )
-    model.fit(dataset_train, dataset_val, batch_size=64)
+    fit(model, dataset_train, dataset_val, batch_size=64)
 
 
 def eval(args):
@@ -252,11 +157,11 @@ def eval(args):
     from .dataset import PI, FSeriesIterableDataset
 
     # torch.random.manual_seed(123)
-    net = RegressionWaveNet.load_from_checkpoint(args.ckpt)
+    net = wave.LitRegressionWaveNet.load_from_checkpoint(args.ckpt).wavenet
     preds = [
         # (NStepPrediction(net), "n-step prediction"),
         (FastGeneration(net), "fast n-step generation"),
-        (NaiveGeneration(net), "naive n-step generation"),
+        # (NaiveGeneration(net), "naive n-step generation"),
     ]
 
     _, dataset_val = dataset.create_default_datasets()
--- autoregressive/wave.py
+++ autoregressive/wave.py
@@ -1,8 +1,11 @@
 from typing import List, Optional
+import logging
+
+import pytorch_lightning as pl
 import torch
 import torch.nn
-import torch.nn.init
 import torch.nn.functional as F
+import torch.nn.init
 
 from .utils import causal_pad
 
@@ -77,18 +80,18 @@ class WaveNetLayer(torch.nn.Module):
         return x_h + x_dilated, skip
 
 
-class WaveNetBackbone(torch.nn.Module):
+class WaveNetBase(torch.nn.Module):
     def __init__(
         self,
         in_channels: int = 1,
         residual_channels: int = 32,
         skip_channels: int = 32,
         num_blocks: int = 1,
-        num_layers: int = 7,
+        num_layers_per_block: int = 7,
     ):
         super().__init__()
         self.conv_input = torch.nn.Conv1d(in_channels, residual_channels, kernel_size=1)
-        self.layers = torch.nn.ModuleList(
+        self.wave_layers = torch.nn.ModuleList(
             [
                 WaveNetLayer(
                     dilation=2 ** d,
@@ -96,36 +99,41 @@ class WaveNetBackbone(torch.nn.Module):
                     skip_channels=skip_channels,
                 )
                 for _ in range(num_blocks)
-                for d in range(num_layers)
+                for d in range(num_layers_per_block)
             ]
         )
         kernel_size = 2
         self.receptive_field = (kernel_size - 1) * sum(
-            [2 ** i for _ in range(num_blocks) for i in range(num_layers)]
+            [2 ** i for _ in range(num_blocks) for i in range(num_layers_per_block)]
         ) + 1
+        self.residual_channels = residual_channels
+        self.skip_channels = skip_channels
+        self.num_blocks = num_blocks
+        self.num_layers_per_block = num_layers_per_block
+        self.in_channels = in_channels
 
-    def forward(self, x):
+    def encode(self, x):
         skips = []
-        outputs = []
+        layer_inputs = []
         x = self.conv_input(x)
-        for layer in self.layers:
-            outputs.append(x)
+        for layer in self.wave_layers:
+            layer_inputs.append(x)
             x, skip = layer(x, fast=False)
             skips.append(skip)
-        return outputs, skips
+        return x, layer_inputs, skips
 
-    def forward_one(self, x, queues: FastQueues):
+    def encode_one(self, x, queues: FastQueues):
         skips = []
-        outputs = []
+        layer_inputs = []
         updated_queues = []
         x = self.conv_input(x)
-        for layer, q in zip(self.layers, queues):
-            outputs.append(x)
+        for layer, q in zip(self.wave_layers, queues):
+            layer_inputs.append(x)
             x, qnew = self._next_input_from_queue(q, x)
             x, skip = layer(x, fast=True)
             updated_queues.append(qnew)
             skips.append(skip)
-        return outputs, skips, updated_queues
+        return x, layer_inputs, skips, updated_queues
 
     def _next_input_from_queue(self, q: torch.Tensor, x):
         h = q[..., 0:1]  # pop left (oldest)
@@ -134,68 +142,158 @@ class WaveNetBackbone(torch.nn.Module):
         x = torch.cat((h, x), -1)  # prepare input
         return x, qout
 
+    def create_fast_queues(
+        self,
+        layer_inputs: Optional[List[torch.FloatTensor]],
+        device: Optional[torch.device] = None,
+    ) -> FastQueues:
+        assert not (layer_inputs is None and device is None)
+        if layer_inputs is None:
+            layer_inputs = [None] * len(self.wave_layers)
+        queues = []
+        for layer, layer_input in zip(self.wave_layers, layer_inputs):
+            layer: WaveNetLayer
+            if layer_input is None:
+                q = torch.zeros(
+                    (1, layer.residual_channels, layer.dilation),
+                    dtype=layer.conv_dilation.weight.dtype,
+                    device=device,
+                )  # That's the same as causal padding
+            else:
+                q = layer_input[..., -layer.dilation :].detach().clone()
+            queues.append(q)
+        return queues
 
-def create_fast_queues(
-    model: WaveNetBackbone,
-    outputs: Optional[List[torch.FloatTensor]],
-    device: Optional[torch.device] = None,
-) -> FastQueues:
-    assert not (outputs is None and device is None)
-    if outputs is None:
-        outputs = [None] * len(model.layers)
-    queues = []
-    for layer, output in zip(model.layers, outputs):
-        layer: WaveNetLayer
-        if output is None:
-            q = torch.zeros(
-                (1, layer.residual_channels, layer.dilation),
-                dtype=layer.conv_dilation.weight.dtype,
-                device=device,
-            )  # That's the same as causal padding
-        else:
-            q = output[..., -layer.dilation :].detach().clone()
-        queues.append(q)
-    return queues
 
-
-class WaveNetLinear(torch.nn.Module):
+class RegressionWaveNet(WaveNetBase):
     def __init__(
         self,
         in_channels: int = 1,
-        out_channels: int = 64,
         residual_channels: int = 32,
         skip_channels: int = 32,
         num_blocks: int = 1,
-        num_layers: int = 7,
+        num_layers_per_block: int = 7,
+        forecast_steps: int = 1,
     ):
-        super().__init__()
-        self.features = WaveNetBackbone(
-            in_channels, residual_channels, skip_channels, num_blocks, num_layers
+        super().__init__(
+            in_channels=in_channels,
+            residual_channels=residual_channels,
+            skip_channels=skip_channels,
+            num_blocks=num_blocks,
+            num_layers_per_block=num_layers_per_block,
         )
+
         self.conv_mid = torch.nn.Conv1d(skip_channels, skip_channels, kernel_size=1)
-        self.conv_output = torch.nn.Conv1d(skip_channels, out_channels, kernel_size=1)
-        self.out_channels = out_channels
+        self.conv_output = torch.nn.Conv1d(skip_channels, forecast_steps, kernel_size=1)
+        self.forecast_steps = forecast_steps
         self.apply(wave_init_weights)
 
-    def forward(self, x, return_outputs: bool = False):
-        outputs, skips = self.features(x)
-        x = torch.stack(skips, dim=0).sum(dim=0)
-        x = F.gelu(x)
-        x = F.gelu(self.conv_mid(x))
-        x = self.conv_output(x)
-        if return_outputs:
-            return x, outputs
-        else:
-            return x
+    def forward(self, x):
+        e, layer_inputs, skips = self.encode(x)
+        x = self._head(e, layer_inputs, skips)
+        return x
 
     def forward_one(self, x, queues: FastQueues):
-        _, skips, queues = self.features.forward_one(x, queues)
+        e, layer_inputs, skips, queues = self.encode_one(x, queues)
+        x = self._head(e, layer_inputs, skips)
+        return x, queues
+
+    def _head(self, encoded, layer_inputs, skips):
+        del encoded, layer_inputs
         x = torch.stack(skips, dim=0).sum(dim=0)
         x = F.gelu(x)
         x = F.gelu(self.conv_mid(x))
         x = self.conv_output(x)
-        return x, queues
+        return x
 
-    @property
-    def receptive_field(self):
-        return self.features.receptive_field
+
+def _exp_weights(T, n):
+    """Returns exponential decaying weights for T terms from 1.0 down to n (n>0)"""
+    n = torch.as_tensor(n).float()
+    x = torch.arange(0, T, 1)
+    b = -torch.log(n) / (T - 1)
+    return torch.exp(-x * b)
+
+
+_logger = logging.getLogger("pytorch_lightning")
+_logger.setLevel(logging.INFO)
+
+
+class LitRegressionWaveNet(pl.LightningModule):
+    def __init__(
+        self,
+        in_channels: int = 1,
+        forecast_steps: int = 64,
+        residual_channels: int = 32,
+        skip_channels: int = 32,
+        num_blocks: int = 1,
+        num_layers_per_block: int = 7,
+        train_full_receptive_field: bool = True,
+        train_exp_decay: bool = False,
+    ) -> None:
+        super().__init__()
+        self.wavenet = RegressionWaveNet(
+            in_channels=in_channels,
+            forecast_steps=forecast_steps,
+            residual_channels=residual_channels,
+            skip_channels=skip_channels,
+            num_blocks=num_blocks,
+            num_layers_per_block=num_layers_per_block,
+        )
+        self.train_full_receptive_field = train_full_receptive_field
+        self.receptive_field = self.wavenet.receptive_field
+        self.train_exp_decay = train_exp_decay
+        if self.train_exp_decay and forecast_steps > 1:
+            self.register_buffer(
+                "l1_weights", _exp_weights(forecast_steps, 1e-3).view(1, -1, 1)
+            )
+        else:
+            self.l1_weights = 1.0
+        _logger.info(f"Receptive field of model {self.wavenet.receptive_field}")
+        super().save_hyperparameters()
+
+    # def forward(self, x, return_outputs: bool = False):
+    #     return self.wave(x, return_outputs=return_outputs)
+
+    # def forward_one(self, x, queues):
+    #     return self.wave.forward_one(x, queues)
+
+    def configure_optimizers(self):
+        import torch.optim.lr_scheduler as sched
+
+        opt = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return {
+            "optimizer": opt,
+            "lr_scheduler": {
+                "scheduler": sched.ReduceLROnPlateau(
+                    opt, mode="min", factor=0.5, patience=1, min_lr=1e-7, threshold=1e-7
+                ),
+                "monitor": "val_loss",
+                "interval": "epoch",
+                "frequency": 1,
+            },
+        }
+
+    def training_step(self, batch, batch_idx):
+        loss = self._step(batch, batch_idx)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self._step(batch, batch_idx)
+        self.log("val_loss", loss, prog_bar=True)
+        return loss
+
+    def _step(self, batch, batch_idx) -> torch.FloatTensor:
+        del batch_idx
+        x = batch["x"][..., :-1].unsqueeze(1)
+        y = batch["xo"][..., 1:]
+        y = y.unfold(-1, self.wavenet.forecast_steps, 1).permute(0, 2, 1)
+        n = y.shape[-1]
+        yhat = self.wavenet(x)
+        r = self.receptive_field if self.train_full_receptive_field else 0
+        losses = F.l1_loss(yhat[..., r:n], y[..., r:], reduction="none")
+        return torch.mean(losses * self.l1_weights)
+
+
+# https://github.com/PyTorchLightning/pytorch-lightning/blob/master/pl_examples/basic_examples/autoencoder.py

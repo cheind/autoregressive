ff8cb252273f0d4addff6a1e8e349283dfc99582
Christoph Heindl <christoph.heindl@gmail.com>
2021-10-27 15:15:53 +0200

    added quantized wave net

--- autoregressive/variants.py
+++ autoregressive/variants.py
@@ -1,6 +1,7 @@
 import logging
 import torch
 import torch.nn.functional as F
+import torch.distributions as D
 
 from . import wave, losses
 
@@ -110,3 +111,80 @@ def regression_sampler(
     # The model directly predicts the values for the next timesteps,
     # so this is the identity function
     return x
+
+
+class QuantizedWaveNet(wave.WaveNetBase):
+    def __init__(
+        self,
+        in_channels: int = 1,
+        residual_channels: int = 64,
+        skip_channels: int = 64,
+        num_blocks: int = 1,
+        num_layers_per_block: int = 9,
+        num_bins: int = 32,
+        train_full_receptive_field: bool = True,
+    ) -> None:
+        super().__init__(
+            in_channels=in_channels,
+            out_channels=num_bins,
+            residual_channels=residual_channels,
+            skip_channels=skip_channels,
+            num_blocks=num_blocks,
+            num_layers_per_block=num_layers_per_block,
+        )
+        self.train_full_receptive_field = train_full_receptive_field
+        self.num_bins = num_bins
+        self.bin_size = 1.0 / (num_bins - 1)
+        super().save_hyperparameters()
+
+    def configure_optimizers(self):
+        import torch.optim.lr_scheduler as sched
+
+        opt = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return {
+            "optimizer": opt,
+            "lr_scheduler": {
+                "scheduler": sched.ReduceLROnPlateau(
+                    opt, mode="min", factor=0.5, patience=2, min_lr=1e-7, threshold=1e-7
+                ),
+                "monitor": "val_loss",
+                "interval": "epoch",
+                "frequency": 1,
+            },
+        }
+
+    def training_step(self, batch, batch_idx):
+        x: torch.Tensor = batch["x"][..., :-1].unsqueeze(1)
+        y: torch.Tensor = batch["b"][..., 1:]
+        logits = self(x)
+        r = self.receptive_field if self.train_full_receptive_field else 0
+        loss = F.cross_entropy(logits[..., r:], y[..., r:])
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x: torch.Tensor = batch["x"][..., :-1].unsqueeze(1)
+        y: torch.Tensor = batch["x"][..., 1:].unsqueeze(1)
+        _, yr, samples, outputs = losses.rolling_nstep(
+            self,
+            self.create_sampler(),
+            x,
+            y,
+            num_forecast=64,
+            max_sequences=512,
+            detach_sample=True,
+        )
+        loss = F.l1_loss(samples, yr)
+        self.log("val_loss", loss, prog_bar=True)
+        return loss
+
+    def create_sampler(self):
+        def sampler(model, obs, x):
+            del model, obs
+            bin = D.Categorical(logits=x.permute(0, 2, 1)).sample()
+            return (bin.float() * self.bin_size).unsqueeze(1)
+
+            # print(x.shape, torch.argmax(x, 1, keepdim=True).shape)
+            # return torch.argmax(x, 1, keepdim=True) * self.bin_size
+
+        return sampler

13b19f61b52b7a575d2003564009c55cbd56a7b0
Christoph Heindl <christoph.heindl@gmail.com>
2021-10-31 08:22:27 +0100

    adds test for minimal wavenet input coverage

--- autoregressive/tests/test_causality.py
+++ autoregressive/tests/test_causality.py
@@ -25,7 +25,7 @@ def test_causal_padding():
     y = cc(x.view(1, 1, -1)).view(-1)
     assert torch.isclose(y[127], x[:128].sum())
     assert torch.isclose(y[255], x[128:].sum())
-    assert (sum([l.conv.weight.numel() for l in layers]), 14)
+    assert sum([l.conv.weight.numel() for l in layers]) == 14
 
 
 def test_data_leakage():
@@ -77,32 +77,61 @@ def test_input_coverage():
 
     model = WaveNetSim(num_blocks=1, num_layers_per_block=3).apply(setup_weights)
     assert model.receptive_field == 8
-    x = torch.arange(0, 32, 1).view(1, 1, -1).float()
-    x = x / x.sum()
+    x = torch.arange(0, 16, 1).view(1, 1, -1).float()
+    assert torch.allclose(
+        model(x),
+        torch.tensor(
+            [
+                [
+                    [
+                        0.0,
+                        1.0,
+                        3.0,
+                        6.0,
+                        10.0,
+                        15.0,
+                        21.0,
+                        28.0,
+                        36.0,
+                        44.0,
+                        52.0,
+                        60.0,
+                        68.0,
+                        76.0,
+                        84.0,
+                        92.0,
+                    ]
+                ]
+            ]
+        ),
+    )
+    model = WaveNetSim(num_blocks=2, num_layers_per_block=2).apply(setup_weights)
+    assert model.receptive_field == 7
+    torch.allclose(
+        model(x),
+        torch.tensor(
+            [
+                [
+                    [
+                        0.0,
+                        1.0,
+                        4.0,
+                        10.0,
+                        20.0,
+                        33.0,
+                        48.0,
+                        64.0,
+                        80.0,
+                        96.0,
+                        112.0,
+                        128.0,
+                        144.0,
+                        160.0,
+                        176.0,
+                        192.0,
+                    ]
+                ]
+            ]
+        ),
+    )
     print(model(x))
-    # self.input_conv = torch.nn.Conv1d(in_channels, wave_channels, kernel_size=1)
-    #     self.wave_layers = torch.nn.ModuleList(
-    #         [
-    #             WaveNetLayer(
-    #                 dilation=2 ** d,
-    #                 wave_channels=wave_channels,
-    #             )
-    #             for _ in range(num_blocks)
-    #             for d in range(num_layers_per_block)
-    #         ]
-    #     )
-
-    # model = wave.WaveNetBase(
-    #     in_channels=1,
-    #     out_channels=1,
-    #     wave_channels=1,
-    #     num_blocks=1,
-    #     num_layers_per_block=3,
-    # ).apply(
-    #     setup_weights
-    # )  # receptive field 7
-
-    # x = torch.arange(0, 32, 1).view(1, 1, -1).float()
-    # encoded, layer_inputs, skips = model.encode(x)
-    # print(encoded)
-    # print(layer_inputs)

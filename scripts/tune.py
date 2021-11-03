a1ffaf97153055d64a2500e75465102bb097cb47
Christoph.Heindl <cheind@profactor.at>
2021-11-03 11:56:25 +0100

    updated script

--- scripts/tune.py
+++ scripts/tune.py
@@ -14,7 +14,15 @@ from ray.tune import CLIReporter
 from autoregressive import datasets, models
 
 
-def train_tune(config, checkpoint_dir=None, max_epochs=30, batch_size=64, num_gpus=1):
+def train_tune(
+    config,
+    checkpoint_dir=None,
+    num_epochs: int = 30,
+    batch_size: int = 64,
+    gpu_per_trial: float = 1.0,
+    train_seed: int = None,
+    val_seed: int = None,
+):
 
     tunecb = TuneReportCheckpointCallback(
         {"loss": "val_loss"}, on="validation_end", filename="checkpoint"
@@ -22,9 +30,9 @@ def train_tune(config, checkpoint_dir=None, max_epochs=30, batch_size=64, num_gp
     lrcb = LearningRateMonitor(logging_interval="step")
 
     kwargs = {
-        "max_epochs": max_epochs,
+        "max_epochs": num_epochs,
         "log_every_n_steps": 25,
-        "gpus": math.ceil(num_gpus),
+        "gpus": math.ceil(gpu_per_trial),
         "logger": TensorBoardLogger(
             save_dir=tune.get_trial_dir(), name="", version="."
         ),
@@ -35,15 +43,21 @@ def train_tune(config, checkpoint_dir=None, max_epochs=30, batch_size=64, num_gp
     if checkpoint_dir:
         kwargs["resume_from_checkpoint"] = os.path.join(checkpoint_dir, "checkpoint")
 
+    train_params = datasets.FSeriesParams(smoothness=0.75, seed=train_seed)
+    val_params = datasets.FSeriesParams(smoothness=0.75, num_curves=512, seed=val_seed)
+    dm = datasets.FSeriesDataModule(
+        batch_size=batch_size,
+        train_fseries_params=train_params,
+        val_fseries_params=val_params,
+    )
+
     model = models.RegressionWaveNet(**config)
-    dm = datasets.FSeriesDataModule(batch_size=batch_size)
+
     trainer = pl.Trainer(**kwargs)
     trainer.fit(model, datamodule=dm)
 
 
-def hypertune(
-    num_samples=30, max_epochs=30, gpus_per_trial=0.5, tune_tag="regression_tune"
-):
+def hypertune(args):
     config = {
         "wave_channels": tune.choice([16, 32, 64]),
         "num_blocks": tune.choice([1, 2]),
@@ -53,8 +67,8 @@ def hypertune(
     }
 
     scheduler = ASHAScheduler(
-        max_t=max_epochs,
-        grace_period=min(10, max_epochs),
+        max_t=args.num_epochs,
+        grace_period=min(10, args.num_epochs),
         reduction_factor=2,
     )
     reporter = CLIReporter(
@@ -67,17 +81,22 @@ def hypertune(
     )
     analysis = tune.run(
         tune.with_parameters(
-            train_tune, max_epochs=max_epochs, num_gpus=gpus_per_trial, batch_size=64
+            train_tune,
+            num_epochs=args.num_epochs,
+            gpu_per_trial=args.gpu_per_trial,
+            train_seed=args.train_seed,
+            val_seed=args.val_seed,
+            batch_size=64,
         ),
-        resources_per_trial={"cpu": 1, "gpu": gpus_per_trial},
+        resources_per_trial={"cpu": 1, "gpu": args.gpu_per_trial},
         metric="loss",
         mode="min",
         config=config,
-        num_samples=num_samples,
+        num_samples=args.num_samples,
         scheduler=scheduler,
         progress_reporter=reporter,
         local_dir="./ray_results",
-        name=tune_tag,
+        name=args.experiment_name,
         trial_dirname_creator=lambda trial: str(trial),
     )
     print("Best hyperparameters found were: ", analysis.best_config)
@@ -86,18 +105,20 @@ def hypertune(
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-num-samples", type=int, default=30)
-    parser.add_argument("-max-epochs", type=int, default=30)
-    parser.add_argument("-name", default="tune_regression")
+    parser.add_argument("-gpu-per-trial", type=float, default=1.0)
+    parser.add_argument("-num-epochs", type=int, default=30)
+    parser.add_argument("-experiment-name", default="tune_regression")
+    parser.add_argument("-train-seed", type=int, default=None)
+    parser.add_argument("-val-seed", type=int, default=None)
     parser.add_argument(
         "--smoke-test", action="store_true", help="Finish quickly for testing"
     )
     args = parser.parse_args()
     if args.smoke_test:
-        hypertune(num_samples=1, max_epochs=1, gpus_per_trial=0, tune_tag=args.name)
-    else:
-        hypertune(
-            num_samples=args.num_samples, max_epochs=args.max_epochs, tune_tag=args.name
-        )
+        args.num_epochs = 1
+        args.num_samples = 1
+        args.gpu_per_trial = 0.0
+    hypertune(args)
 
 
 if __name__ == "__main__":

8e787edd10e080afbcd77a1f1e3d251710208e0a
Christoph Heindl <christoph.heindl@gmail.com>
2021-11-08 17:46:40 +0100

    refactored functional

--- autoregressive/datasets/fseries_synthetic.py
+++ autoregressive/datasets/fseries_synthetic.py
@@ -8,16 +8,82 @@ from collections.abc import Sequence
 from typing import Any, Callable, Dict, Tuple, Union
 import dataclasses
 
+import numpy as np
 import torch
 import torch.utils.data
 import pytorch_lightning as pl
 
 from .common import SeriesDataset
-from . import transforms, functional
 
 Sample = Dict[str, Any]
 FloatOrFloatRange = Union[float, Tuple[float, float]]
 IntOrIntRange = Union[int, Tuple[int, int]]
+
+PI = float(np.pi)
+
+
+def fseries_amp_phase(
+    bias: torch.FloatTensor,
+    n: torch.IntTensor,
+    a: torch.FloatTensor,
+    phase: torch.FloatTensor,
+    period: torch.FloatTensor,
+    t: torch.FloatTensor,
+):
+    """Computes the Fourier series from amplitude-phase parametrization.
+
+    This function supports batching, so that multiple series can
+    be evaluated in parallel.
+
+    Params
+    ------
+    bias: (B,) or (1) tensor
+        Bias term(s) aka DC values
+    n: (B,N) or (N,) or tensor
+        component values of the series
+    a: (B,N) or (N,) tensor
+        coefficient of components
+    phase: (B,N) or (B,) or (1) tensor
+        phase for component term
+    period: (B,) or (1) tensor
+        period for each of the curves, such that f(t) == f(t+T)
+    t: (B,T) or (T,) tensor
+        sample times for each of the curves
+
+    Returns
+    -------
+    y: (B,T) tensor or (1,T)
+        function values for each of the fourier series results
+    """
+    # https://www.seas.upenn.edu/~kassam/tcom370/n99_2B.pdf
+    t = torch.atleast_2d(t).unsqueeze(1)  # (B,1,T)
+    period = torch.atleast_1d(period).view(-1, 1, 1)  # (B,1,1)
+    n = torch.atleast_2d(n).unsqueeze(-1)  # (B,N,1)
+    phase = torch.atleast_2d(phase).unsqueeze(-1)  # (B,N,1)
+    bias = torch.atleast_1d(bias).unsqueeze(-1)  # (B,1)
+    a = torch.atleast_2d(a).unsqueeze(1)  # (B,1,N)
+
+    # print(t.shape, n.shape, phase.shape, period.shape)
+
+    f0 = 1 / period
+    arg = 2 * PI * f0 * n * t + phase  # (B,N,T)
+    return bias * 0.5 + (a @ torch.cos(arg)).squeeze(1)
+
+
+@dataclasses.dataclass
+class FSeriesParams:
+    num_curves: int = 8096
+    num_tsamples: int = 2048
+    dt: float = 0.02
+    fterm_range: IntOrIntRange = (3, 5)
+    tstart_range: FloatOrFloatRange = 0.0
+    period_range: FloatOrFloatRange = 10.0
+    bias_range: FloatOrFloatRange = 0.0
+    coeff_range: FloatOrFloatRange = (-1.0, 1.0)
+    phase_range: FloatOrFloatRange = (-PI, PI)
+    lineartrend_range: FloatOrFloatRange = 0.0
+    smoothness: float = 0.0
+    seed: int = None
 
 
 class FSeriesDataset(SeriesDataset):
@@ -25,20 +91,8 @@ class FSeriesDataset(SeriesDataset):
 
     def __init__(
         self,
-        num_curves: int = 8096,
-        num_tsamples: int = 500,
-        dt: float = 0.02,
-        fterm_range: IntOrIntRange = 3,
-        tstart_range: FloatOrFloatRange = 0.0,
-        period_range: FloatOrFloatRange = 10.0,
-        bias_range: FloatOrFloatRange = 0.0,
-        coeff_range: FloatOrFloatRange = (-1.0, 1.0),
-        phase_range: FloatOrFloatRange = (-functional.PI, functional.PI),
-        lineartrend_range: FloatOrFloatRange = 0.0,
-        smoothness: float = 0.0,
+        params: FSeriesParams,
         transform: Callable[[Sample], Sample] = None,
-        seed: int = None,
-        include_params: bool = False,
     ) -> None:
         super().__init__()
         eps = torch.finfo(torch.float32).eps
@@ -48,24 +102,24 @@ class FSeriesDataset(SeriesDataset):
                 arg = (arg, arg + delta)
             return arg
 
-        self.num_curves = num_curves
-        self.fterm_range = _make_range(fterm_range, 1)
-        self.period_range = _make_range(period_range, eps)
-        self.bias_range = _make_range(bias_range, eps)
-        self.coeff_range = _make_range(coeff_range, eps)
-        self.phase_range = _make_range(phase_range, eps)
-        self.tstart_range = _make_range(tstart_range, eps)
-        self.lineartrend_range = _make_range(lineartrend_range, eps)
-        self.num_tsamples = num_tsamples
-        self.dt = dt
-        self.smoothness = smoothness
+        self.num_curves = params.num_curves
+        self.fterm_range = _make_range(params.fterm_range, 1)
+        self.period_range = _make_range(params.period_range, eps)
+        self.bias_range = _make_range(params.bias_range, eps)
+        self.coeff_range = _make_range(params.coeff_range, eps)
+        self.phase_range = _make_range(params.phase_range, eps)
+        self.tstart_range = _make_range(params.tstart_range, eps)
+        self.lineartrend_range = _make_range(params.lineartrend_range, eps)
+        self.num_tsamples = params.num_tsamples
+        self.dt = params.dt
+        self.smoothness = params.smoothness
         self.transform = transform
-        self.include_params = include_params
-        if seed is None:
+        self.include_params = params.include_params
+        if params.seed is None:
             rng = torch.default_generator
         else:
-            rng = torch.Generator().manual_seed(seed)
-        self.curve_params = [self._sample_params(rng) for _ in range(num_curves)]
+            rng = torch.Generator().manual_seed(params.seed)
+        self.curve_params = [self._sample_params(rng) for _ in range(self.num_curves)]
 
     def __len__(self):
         return self.num_curves
@@ -75,9 +129,7 @@ class FSeriesDataset(SeriesDataset):
         p = self.curve_params[index]
         t = torch.arange(p["tstart"], self.dt * self.num_tsamples, self.dt)
         n = torch.arange(p["terms"]) + 1
-        x = functional.fseries_amp_phase(
-            p["bias"], n, p["coeffs"], p["phase"], p["period"], t
-        )[0]
+        x = fseries_amp_phase(p["bias"], n, p["coeffs"], p["phase"], p["period"], t)[0]
         x += t * p["lineark"]
         sample = {"x": x, "xo": x.clone(), "t": t}
         if self.include_params:
@@ -116,23 +168,6 @@ class FSeriesDataset(SeriesDataset):
         }
 
 
-@dataclasses.dataclass
-class FSeriesParams:
-    num_curves: int = 8096
-    num_tsamples: int = 2048
-    dt: float = 0.02
-    fterm_range: IntOrIntRange = (3, 5)
-    tstart_range: FloatOrFloatRange = 0.0
-    period_range: FloatOrFloatRange = 10.0
-    bias_range: FloatOrFloatRange = 0.0
-    coeff_range: FloatOrFloatRange = (-1.0, 1.0)
-    phase_range: FloatOrFloatRange = (-functional.PI, functional.PI)
-    lineartrend_range: FloatOrFloatRange = 0.0
-    smoothness: float = 0.0
-    transform: Callable[[Sample], Sample] = None
-    seed: int = None
-
-
 class FSeriesDataModule(pl.LightningDataModule):
     def __init__(
         self,
@@ -146,8 +181,8 @@ class FSeriesDataModule(pl.LightningDataModule):
         super().__init__()
         self.train_fseries_params = train_fseries_params
         self.val_fseries_params = val_fseries_params
-        self.train_ds = FSeriesDataset(**dataclasses.asdict(train_fseries_params))
-        self.val_ds = FSeriesDataset(**dataclasses.asdict(val_fseries_params))
+        self.train_ds = FSeriesDataset(train_fseries_params)
+        self.val_ds = FSeriesDataset(val_fseries_params)
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.dt = self.train_ds.dt
@@ -173,25 +208,87 @@ class FSeriesDataModule(pl.LightningDataModule):
         return f"train_params: {self.train_fseries_params}\n val_params:{self.val_fseries_params}"
 
 
-def main():
+def square_wave():
+    # https://mathworld.wolfram.com/FourierSeriesSquareWave.html
     import matplotlib.pyplot as plt
-    from mpl_toolkits.axes_grid1 import ImageGrid
 
-    # dm = FSeriesDataModule(
-    #     train_fseries_params=FSeriesParams(smoothness=0.75), batch_size=512
-    # )
-    dm = FSeriesDataModule()
+    T = 10.0
+    num_terms = 20
+    num_samples = 1000
+    n = torch.arange(1, 2 * num_terms, step=2)
+    coeffs = 4.0 / (PI * n)
+    # We generate multiple (B=4) approximations with increasing frequencies enabled.
+    coeffs = coeffs.view(1, -1).repeat(4, 1)
+    coeffs[0, 5:] = 0.0  # For the first curve, we disable all but the first 5 terms
+    coeffs[1, 10:] = 0.0
+    coeffs[2, 15:] = 0.0
+    phase = torch.tensor(-PI / 2)  # We share phase angles (sin(phi) = cos(phi-pi/2))
+    bias = torch.tensor(0.0)  # We don't have a bias aka DC
+    t = torch.linspace(
+        0, T, num_samples
+    )  # We sample all curves at the same time intervals
 
-    fig = plt.figure(figsize=(8.0, 8.0))
-    grid = ImageGrid(
-        fig, 111, nrows_ncols=(5, 5), axes_pad=0.05, share_all=False, aspect=False
+    y = fseries_amp_phase(
+        bias=bias,
+        n=n,
+        a=coeffs,
+        phase=phase,
+        period=torch.tensor(T),
+        t=t,
     )
+    yhat = torch.zeros(len(t))
+    yhat[: num_samples // 2] = 1.0
+    yhat[num_samples // 2 :] = -1.0
+    yhat[0] = 0.0
+    yhat[-1] = 0.0
 
-    for ax, s in zip(grid, dm.train_ds):
-        # ax.step(s["t"], s["x"])
-        ax.scatter(s["t"], s["x"], s=1.0)
-        ax.set_ylim(0, 1.0)
+    plt.title("Approximations of a step function")
+    plt.plot(t, yhat, c="k")
+    plt.plot(t, y[0], label="5 terms")
+    plt.plot(t, y[1], label="10 terms")
+    plt.plot(t, y[2], label="15 terms")
+    plt.plot(t, y[3], label="20 terms")
+    plt.legend()
     plt.show()
+
+
+def random_waves():
+    import matplotlib.pyplot as plt
+
+    N = 5
+    n = torch.arange(N)
+    coeffs = torch.rand(4, N)
+    phase = torch.rand(4, N)
+    bias = torch.rand(4)
+    period = torch.rand(4) * 5
+    t = torch.stack(
+        [
+            torch.linspace(0, 1, 1000),
+            torch.linspace(0.5, 1.5, 1000),
+            torch.linspace(1.5, 2.5, 1000),
+            torch.linspace(2.5, 3.5, 1000),
+        ],
+        0,
+    )
+    y = fseries_amp_phase(
+        bias=bias,
+        n=n,
+        a=coeffs,
+        phase=phase,
+        period=period,
+        t=t,
+    )
+    plt.title("Random waves sampled at random times")
+    plt.plot(t[0], y[0])
+    plt.plot(t[1], y[1])
+    plt.plot(t[2], y[2])
+    plt.plot(t[3], y[3])
+    plt.show()
+
+
+def main():
+    square_wave()
+    random_waves()
 
 
 if __name__ == "__main__":
--- autoregressive/datasets/functional.py
+++ autoregressive/datasets/functional.py
@@ -1,65 +1,10 @@
 __all__ = [
-    "fseries_amp_phase",
     "find_series_range",
-    "find_series_mean_std",
     "normalize_series",
     "normalize_series_inv",
-    "standardize_series",
-    "standardize_series_inv",
 ]
 from typing import Tuple, Iterable
-import numpy as np
 import torch
-
-PI = float(np.pi)
-
-
-def fseries_amp_phase(
-    bias: torch.FloatTensor,
-    n: torch.IntTensor,
-    a: torch.FloatTensor,
-    phase: torch.FloatTensor,
-    period: torch.FloatTensor,
-    t: torch.FloatTensor,
-):
-    """Computes the Fourier series from amplitude-phase parametrization.
-
-    This function supports batching, so that multiple series can
-    be evaluated in parallel.
-
-    Params
-    ------
-    bias: (B,) or (1) tensor
-        Bias term(s) aka DC values
-    n: (B,N) or (N,) or tensor
-        component values of the series
-    a: (B,N) or (N,) tensor
-        coefficient of components
-    phase: (B,N) or (B,) or (1) tensor
-        phase for component term
-    period: (B,) or (1) tensor
-        period for each of the curves, such that f(t) == f(t+T)
-    t: (B,T) or (T,) tensor
-        sample times for each of the curves
-
-    Returns
-    -------
-    y: (B,T) tensor or (1,T)
-        function values for each of the fourier series results
-    """
-    # https://www.seas.upenn.edu/~kassam/tcom370/n99_2B.pdf
-    t = torch.atleast_2d(t).unsqueeze(1)  # (B,1,T)
-    period = torch.atleast_1d(period).view(-1, 1, 1)  # (B,1,1)
-    n = torch.atleast_2d(n).unsqueeze(-1)  # (B,N,1)
-    phase = torch.atleast_2d(phase).unsqueeze(-1)  # (B,N,1)
-    bias = torch.atleast_1d(bias).unsqueeze(-1)  # (B,1)
-    a = torch.atleast_2d(a).unsqueeze(1)  # (B,1,N)
-
-    # print(t.shape, n.shape, phase.shape, period.shape)
-
-    f0 = 1 / period
-    arg = 2 * PI * f0 * n * t + phase  # (B,N,T)
-    return bias * 0.5 + (a @ torch.cos(arg)).squeeze(1)
 
 
 def find_series_range(series: Iterable[torch.Tensor]) -> Tuple[float, float]:
@@ -70,13 +15,6 @@ def find_series_range(series: Iterable[torch.Tensor]) -> Tuple[float, float]:
         lower = min(lower, s.min().item())
         upper = max(upper, s.max().item())
     return lower, upper
-
-
-def find_series_mean_std(series: Iterable[torch.Tensor]) -> Tuple[float, float]:
-    """Returns mean, std of series"""
-    if not isinstance(series, torch.Tensor):
-        series = torch.stack(series, 0)
-    return series.mean(), series.std()
 
 
 def normalize_series(
@@ -99,100 +37,3 @@ def normalize_series_inv(
 ) -> torch.Tensor:
     """(Batch) un-normalize a given series."""
     return normalize_series(x, target_range, source_range)
-
-
-def standardize_series(x: torch.Tensor, mean: float, std: float) -> torch.Tensor:
-    """(Batch) standardize a given series."""
-    return (x - mean) / std
-
-
-def standardize_series_inv(x: torch.Tensor, mean: float, std: float) -> torch.Tensor:
-    """(Batch) inverse standardize of time series."""
-    return x * std + mean
-
-
-def square_wave():
-    # https://mathworld.wolfram.com/FourierSeriesSquareWave.html
-    import matplotlib.pyplot as plt
-
-    T = 10.0
-    num_terms = 20
-    num_samples = 1000
-    n = torch.arange(1, 2 * num_terms, step=2)
-    coeffs = 4.0 / (PI * n)
-    # We generate multiple (B=4) approximations with increasing frequencies enabled.
-    coeffs = coeffs.view(1, -1).repeat(4, 1)
-    coeffs[0, 5:] = 0.0  # For the first curve, we disable all but the first 5 terms
-    coeffs[1, 10:] = 0.0
-    coeffs[2, 15:] = 0.0
-    phase = torch.tensor(-PI / 2)  # We share phase angles (sin(phi) = cos(phi-pi/2))
-    bias = torch.tensor(0.0)  # We don't have a bias aka DC
-    t = torch.linspace(
-        0, T, num_samples
-    )  # We sample all curves at the same time intervals
-
-    y = fseries_amp_phase(
-        bias=bias,
-        n=n,
-        a=coeffs,
-        phase=phase,
-        period=torch.tensor(T),
-        t=t,
-    )
-    yhat = torch.zeros(len(t))
-    yhat[: num_samples // 2] = 1.0
-    yhat[num_samples // 2 :] = -1.0
-    yhat[0] = 0.0
-    yhat[-1] = 0.0
-
-    plt.title("Approximations of a step function")
-    plt.plot(t, yhat, c="k")
-    plt.plot(t, y[0], label="5 terms")
-    plt.plot(t, y[1], label="10 terms")
-    plt.plot(t, y[2], label="15 terms")
-    plt.plot(t, y[3], label="20 terms")
-    plt.legend()
-    plt.show()
-
-
-def random_waves():
-    import matplotlib.pyplot as plt
-
-    N = 5
-    n = torch.arange(N)
-    coeffs = torch.rand(4, N)
-    phase = torch.rand(4, N)
-    bias = torch.rand(4)
-    period = torch.rand(4) * 5
-    t = torch.stack(
-        [
-            torch.linspace(0, 1, 1000),
-            torch.linspace(0.5, 1.5, 1000),
-            torch.linspace(1.5, 2.5, 1000),
-            torch.linspace(2.5, 3.5, 1000),
-        ],
-        0,
-    )
-    y = fseries_amp_phase(
-        bias=bias,
-        n=n,
-        a=coeffs,
-        phase=phase,
-        period=period,
-        t=t,
-    )
-    plt.title("Random waves sampled at random times")
-    plt.plot(t[0], y[0])
-    plt.plot(t[1], y[1])
-    plt.plot(t[2], y[2])
-    plt.plot(t[3], y[3])
-    plt.show()
-
-
-def main():
-    square_wave()
-    random_waves()
-
-
-if __name__ == "__main__":
-    main()

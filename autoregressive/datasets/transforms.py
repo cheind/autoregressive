de39e619078e5b99f23c8784f199521067bdbfd3
Christoph.Heindl <cheind@profactor.at>
2021-11-10 11:57:13 +0100

    simplified transforms

--- autoregressive/datasets/transforms.py
+++ autoregressive/datasets/transforms.py
@@ -1,18 +1,37 @@
-__all__ = ["Encode", "chain_transforms"]
-
+__all__ = ["Encode", "EncodeParams", "chain_transforms"]
+import dataclasses
 from .. import signal
 from .series_dataset import Series
+
+
+@dataclasses.dataclass
+class EncodeParams:
+    num_levels: int
+    input_range: tuple[float, float]
+    bin_shift: bool = True
+    one_hot: bool = False
 
 
 class Encode:
     """Transform to perform normalization and quantization of series data."""
 
-    def __init__(self, params: signal.EncoderParams):
-        self.encdec = signal.EncoderDecoder(params)
+    def __init__(
+        self,
+        num_levels: int,
+        input_range: tuple[float, float],
+        bin_shift: bool = True,
+        one_hot: bool = False,
+    ):
+        self.encdec = signal.EncoderDecoder(
+            num_levels=num_levels,
+            input_range=input_range,
+            bin_shift=bin_shift,
+            one_hot=one_hot,
+        )
 
     def __call__(self, series: Series) -> Series:
-        k = self.encdec.encode(series["x"])
-        series["x_k"] = k
+        series["x_k"] = self.encdec.encode(series["x"])
+        return series
 
 
 def chain_transforms(*args):

2237ee9c7ccd712ab3267987cf5aa8ae91ffa392
Christoph.Heindl <cheind@profactor.at>
2021-11-10 09:46:33 +0100

    updated measures

--- autoregressive/tests/test_measures.py
+++ autoregressive/tests/test_measures.py
@@ -1,7 +1,6 @@
 import torch
-from torch.utils.data import DataLoader
 
-from .. import measures, datasets
+from .. import measures
 
 
 def test_sample_entropy():
@@ -19,21 +18,3 @@ def test_sample_entropy():
     x = torch.sin(torch.linspace(0, 10 * 3.145, 2 ** 12))
     se = measures.sample_entropy(x).mean()
     assert se < 0.2
-
-    # ds = datasets.FSeriesDataset(period_range=10, seed=123)
-    # b = next(iter(DataLoader(ds, batch_size=512)))
-    # se_fixed_p = measures.sample_entropy(b["x"]).mean()
-
-    # ds = datasets.FSeriesDataset(period_range=(5, 10), seed=123)
-    # b = next(iter(DataLoader(ds, batch_size=512)))
-    # se_var_p = measures.sample_entropy(b["x"]).mean()
-
-    # ds = datasets.FSeriesDataset(
-    #     period_range=(5, 10),
-    #     seed=123,
-    #     transform=datasets.transforms.Noise(scale=1e-1, p=1),
-    # )
-    # b = next(iter(DataLoader(ds, batch_size=512)))
-    # se_var_p_noise = measures.sample_entropy(b["x"]).mean()
-
-    # assert se_var_p_noise > se_var_p > se_fixed_p

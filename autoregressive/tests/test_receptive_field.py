03c7876a9812ba161b25cf6939355598b22aef85
Christoph Heindl <christoph.heindl@gmail.com>
2021-11-10 06:41:13 +0100

    removed obsolete files

--- autoregressive/tests/test_receptive_field.py
+++
@@ -1,9 +0,0 @@
-from ..wave import compute_receptive_field
-
-
-def test_receptive_field():
-    assert compute_receptive_field(dilation_seq=[1]) == 2
-    assert compute_receptive_field(dilation_seq=[1, 2]) == 4
-    assert compute_receptive_field(dilation_seq=[1, 2, 4]) == 8
-    assert compute_receptive_field(dilation_seq=[1, 2, 4, 1]) == 9
-    assert compute_receptive_field(dilation_seq=[1, 2, 4, 1, 2, 4]) == 15

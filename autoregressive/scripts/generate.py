d0b3070d1fbade1d1ff741f6dc4ff78b74b7c05b
Christoph.Heindl <cheind@profactor.at>
2021-11-12 11:04:08 +0100

    updated example

--- autoregressive/scripts/generate.py
+++ autoregressive/scripts/generate.py
@@ -10,7 +10,7 @@ from mpl_toolkits.axes_grid1 import ImageGrid
 from pytorch_lightning.utilities.cli import LightningCLI
 
 
-from autoregressive import wave, generators
+from autoregressive import wave, generators, signal, datasets
 
 
 def geometry(arg: str) -> Tuple[int, int]:
@@ -24,10 +24,11 @@ class InstantiateOnlyLightningCLI(LightningCLI):
 
 class GenerateLightningCLI(InstantiateOnlyLightningCLI):
     def add_arguments_to_parser(self, parser) -> None:
-        parser.add_argument("-num-traj", type=int, default=1)
-        parser.add_argument("-num-steps", type=int, default=31)
-        parser.add_argument("-shift", type=int, default=0)
+        parser.add_argument("-samples", type=int, default=1)
+        parser.add_argument("-horizon", type=int, default=None)
+        parser.add_argument("-initial-obs", type=int)
         parser.add_argument("-curves", default="4x1", metavar="ROWSxCOLS")
+        parser.add_argument("-noise-scale", type=float)
         parser.add_argument(
             "--fast-wavenet", action=argparse.BooleanOptionalAction, default=True
         )
@@ -60,9 +61,15 @@ def main():
 
     dm = cli.datamodule
     ds = dm.val_ds
+    if cfg["noise_scale"] is not None:
+        ds.transform = datasets.transforms.chain_transforms(
+            datasets.transforms.Noise(scale=cfg["noise_scale"]), ds.transform
+        )
+
     # ds.transform = datasets.Noise(scale=1e-1, p=1.0)
     # S = min(model.receptive_field + cfg["shift"], ds[0]["x"].shape[-1])
-    S = 1
+    R = cfg["initial_obs"] or model.receptive_field
+    steps = cfg["horizon"] or ds[0]["x_k"].shape[-1] - R
 
     fig = plt.figure()
     grid = ImageGrid(
@@ -71,7 +78,7 @@ def main():
         nrows_ncols=curve_layout,
         axes_pad=0.05,
         share_all=True,
-        label_mode="1",
+        # label_mode="1",
         aspect=True,
     )
     grid[0].get_yaxis().set_ticks([])
@@ -84,7 +91,7 @@ def main():
     # these observations for the number of trajectories
     obs = torch.stack([c["x_k"] for c in curves], 0)  # (B,T) or (B,Q,T)
     rep = [1] * obs.dim()
-    rep[0] = cfg["num_traj"]
+    rep[0] = cfg["samples"]
     obs = obs.repeat(*rep).to(dev)
 
     # Generate. Note, we leave the last obs out so the first item predicted
@@ -92,49 +99,64 @@ def main():
     if cfg["fast_wavenet"]:
         g = generators.generate_fast(
             model=model,
-            initial_obs=obs[..., :S],
+            initial_obs=obs[..., :R],
             sampler=sampler,
         )
     else:
         g = generators.generate(
             model=model,
-            initial_obs=obs[..., :S],
+            initial_obs=obs[..., :R],
             sampler=sampler,
         )
 
     t0 = time.time()
-    trajs, _ = generators.slice_generator(g, stop=cfg["num_steps"])  # (B,1,T)
+
+    trajs, _ = generators.slice_generator(g, stop=steps)  # (B,1,T)
     trajs = trajs.squeeze(1).cpu()
-    tn = torch.arange(S, S + cfg["num_steps"], 1)
+    tn = torch.arange(R, R + steps, 1) * dm.dt
+    tx = torch.arange(0, R + steps, 1) * dm.dt
     print(f"Generation took {(time.time()-t0):.3f} secs")
 
     # Plot
     for idx, (ax, s) in enumerate(zip(grid, curves)):
-        x = s["x_k"]
-        ax.step(
-            torch.arange(0, len(x), 1),
-            x,
-            c="k",
-            linewidth=0.5,
-            label="input",
-            linestyle="--",
+        ed = signal.EncoderDecoder(
+            s["encode.num_levels"],
+            s["encode.input_range"],
+            s["encode.bin_shift"],
+            s["encode.one_hot"],
         )
-        # ax.step(t, xo, c="k", linewidth=0.5, label="input", linestyle="--")
-        # ax.scatter(
-        #     t, x.argmax(0), c="k", s=10, linewidth=0.5, label="input", linestyle="--"
-        # )
+        if "x" in s:
+            ax.plot(
+                tx,
+                s["x"][..., : R + steps],
+                c="k",
+                linewidth=0.5,
+                label="input",
+                linestyle="--",
+                alpha=0.3,
+            )
+        else:
+            ax.step(
+                tx,
+                ed.decode(s["x_k"])[..., : R + steps],
+                c="k",
+                linewidth=0.5,
+                label="input",
+                linestyle="--",
+                alpha=0.3,
+            )
 
         for tidx, xn in enumerate(trajs[idx::num_curves]):
             # Note, above we step with num_curves to get all trajectories
             # for this axis. Related to repeat statement above.
-            ax.step(tn, xn, label="generated" if tidx == 0 else "")
-        # ax.set_ylim(-3, 3)
+            ax.step(tn, ed.decode(xn), label="generated" if tidx == 0 else "")
+        r = s["encode.input_range"]
+        ax.set_ylim(r[0], r[1])
 
     handles, labels = ax.get_legend_handles_labels()
-    fig.legend(handles, labels, loc="upper left")
-    fig.suptitle(f"Sample generation by {type(model).__name__}")
-    # fig.tight_layout()
-    fig.savefig(f"tmp/generate_{type(model).__name__}.pdf")
+    fig.legend(handles, labels, loc="upper center", ncol=2)
+    fig.tight_layout()
+    fig.savefig(f"tmp/generate_{type(model).__name__}.svg", bbox_inches="tight")
     plt.show()
 
 

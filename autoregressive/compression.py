b850c608b0c208a12c6e9a25dd63c75cdf6249e9
Christoph.Heindl <cheind@profactor.at>
2021-11-12 07:17:43 +0100

    ensure input is fload

--- autoregressive/compression.py
+++ autoregressive/compression.py
@@ -7,5 +7,5 @@ def to_one_hot(x: torch.Tensor, num_classes: int):
     if x.dim() == 2:
         # compressed encoding (B,T) -> one encoding (B,Q,T)
         x = F.one_hot(x, num_classes=num_classes)  # (B,T,Q)
-        x = x.permute(0, 2, 1).float()  # (B,Q,T)
-    return x
+        x = x.permute(0, 2, 1)  # (B,Q,T)
+    return x.float()

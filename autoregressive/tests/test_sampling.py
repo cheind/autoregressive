03b54ef7700d072e576a6173905afba8e36a61b0
Christoph.Heindl <cheind@profactor.at>
2021-11-12 07:00:50 +0100

    updated docs

--- autoregressive/tests/test_sampling.py
+++ autoregressive/tests/test_sampling.py
@@ -6,7 +6,7 @@ from scipy.stats import chi2
 from .. import sampling
 
 
-def chi2_test(s: torch.Tensor, pi: torch.Tensor, p=0.05) -> bool:
+def _chi2test(s: torch.Tensor, pi: torch.Tensor, p=0.05) -> bool:
     """Returns true if the null hypothesis of bin-probabilities pi for observations s is accepted."""
     # Number of categories
     K = len(pi)
@@ -19,13 +19,13 @@ def chi2_test(s: torch.Tensor, pi: torch.Tensor, p=0.05) -> bool:
     # Computed the expected frequencies under h0
     expected = pi * N
     print(x, expected)
-    # Compute the statistic which is approx. chi2 distributed.
+    # Compute the statistic which (approx. chi2 distributed)
     chi2_value = ((x - expected) ** 2 / expected).sum()
-    # Compute the threshold
+    # Compute the p-value, i.e P(X >= chi2_value)
     chi2_crit = chi2.ppf(1.0 - p, df=K - 1)
     # If the probability of observing a chi2_value or something more extreme
     # is less than p we reject h0
-    print(chi2_value.item(), chi2_crit)
+    # print(chi2_value.item(), chi2_crit)
     return chi2_value <= chi2_crit
 
 
@@ -34,13 +34,13 @@ def test_greedy_sampler():
     logits = torch.rand(2, 10, 5000)
     samples = sampling.GreedySampler()(logits)
     assert samples.shape == (2, 5000)
-    assert chi2_test(samples.view(-1), torch.ones(10) * 0.1)
+    assert _chi2test(samples.view(-1), torch.ones(10) * 0.1)
 
     logits = torch.rand(2, 10, 10000)
     logits[:, :2, :] += 0.1
     samples = sampling.GreedySampler()(logits)
-    assert not chi2_test(samples.view(-1), torch.ones(10) * 0.1)
-    assert chi2_test(
+    assert not _chi2test(samples.view(-1), torch.ones(10) * 0.1)
+    assert _chi2test(
         samples.view(-1),
         torch.tensor([0.18, 0.18, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08]),
     )
@@ -51,7 +51,7 @@ def test_stochastic_sampler():
     logits = torch.tensor([1.0, 2.0, 1.0, 1.0]).view(1, 4, 1).repeat(1, 1, 20000)
     samples = sampling.StochasticSampler()(logits)
     assert samples.shape == (1, 20000)
-    assert chi2_test(
+    assert _chi2test(
         samples.view(-1), torch.tensor([3500, 9400, 3500, 3500]) / 20000, p=0.01
     )
 

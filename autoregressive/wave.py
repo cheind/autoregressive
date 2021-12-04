3beaafb8d9b1f331ae0818159348b96a06a3a1e1
Christoph Heindl <christoph.heindl@gmail.com>
2021-12-04 10:12:37 +0100

    updated code

--- autoregressive/wave.py
+++ autoregressive/wave.py
@@ -355,12 +355,12 @@ class WaveNet(pl.LightningModule):
             if cond.shape[-1] == series["x"].shape[-1]:
                 # local conditioning - cut to same length as inputs
                 cond = cond[..., :-1]
-                # As a test we add cond directly to input
-                inputs = encoding.one_hotf(
-                    inputs, quantization_levels=self.quantization_levels
-                )
-                inputs = torch.cat((inputs, cond), 1)
-                cond = None
+                # # As a test we add cond directly to input
+                # inputs = encoding.one_hotf(
+                #     inputs, quantization_levels=self.quantization_levels
+                # )
+                # inputs = torch.cat((inputs, cond), 1)
+                # cond = None
 
         if horizon == 1:
             logits, _ = self.forward(inputs, c=cond)  # supports local cond as well

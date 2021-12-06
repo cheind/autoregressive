d8a9990850e724f5a6b84e5bf247670574e24df6
Christoph Heindl <christoph.heindl@gmail.com>
2021-12-06 19:37:50 +0100

    added density estimation example

--- autoregressive/scripts/mnist.py
+++ autoregressive/scripts/mnist.py
@@ -9,6 +9,56 @@ from torchvision.utils import make_grid
 from .. import datasets, generators, sampling, wave
 
 
+def load_images_targets(data: datasets.MNISTDataModule, n: int, seed: int = None):
+    ds = data.test_dataloader().dataset
+    g = torch.default_generator
+    if seed is not None:
+        g = torch.Generator().manual_seed(seed)
+    ids = torch.randint(0, len(ds), (n,), generator=g)
+
+    imgs = []
+    targets = []
+
+    for idx in ids:
+        sm = ds[idx]
+        imgs.append(sm[0]["x"])
+        targets.append(sm[1]["digit"])
+
+    return imgs, targets
+
+
+@torch.no_grad()
+def compute_log_pxy(imgs: torch.Tensor, model: wave.WaveNet):
+    """Computes p(X=x|Y) for each possible value of y as a BxQ tensor"""
+    # log p(X=x) = log sum_y p(X=x|Y=y)p(Y=y)
+    #            = log sum_y exp(log p(X=x|Y=y)p(Y=y))
+    #            = log sum_y exp(log p(X=x|Y=y) + log p(Y=y))
+    #            = log sum_y exp(log p(X0|Y=y)p(X1|X0,Y=y)...p(XT|XT-1...X0,Y=y) + log p(Y=y))
+    # this can be conveniently computed using torch.logsumexp
+    Y = model.conditioning_channels
+    Q = model.quantization_levels
+    B, T = imgs.shape
+    log_py = torch.log(torch.tensor(1.0 / 10))
+
+    y_conds = (
+        (F.one_hot(torch.arange(0, Y, 1), num_classes=Y).permute(0, 1).view(Y, Y, 1))
+        .to(imgs.device)
+        .float()
+    )
+
+    # imgs is (B,T)
+    log_pxys = []
+    for y in range(10):
+        logits, _ = model.forward(x=imgs, c=y_conds[y : y + 1])  # (B,Q,T)
+        log_px_y = F.log_softmax(logits, 1)
+        log_px_y = log_px_y.permute(0, 2, 1).reshape(-1, Q)  # (B*T,Q)
+        log_px_y = log_px_y[torch.arange(B * T), imgs.view(-1)].view(B, T)  # (B,T)
+        log_pxy = log_px_y.sum(-1) + log_py
+        log_pxys.append(log_pxy)
+        del logits
+    return torch.stack(log_pxys, -1)
+
+
 class SampleDigitsCommand:
     """Samples from the condition distribution p(x|digit) where x is an mnist image."""
 
@@ -93,25 +143,11 @@ class InfillDigitsCommand:
         self.data = datasets.MNISTDataModule(**plcfg["data"]["init_args"])
         self.model = wave.WaveNet.load_from_checkpoint(ckpt).eval()
 
-    def _load_images_targets(self):
-        ds = self.data.test_dataloader().dataset
-        ids = torch.randint(0, len(ds), (self.num_images,))
-
-        imgs = []
-        targets = []
-
-        for idx in ids:
-            sm = ds[idx]
-            imgs.append(sm[0]["x"])
-            targets.append(sm[1]["digit"])
-
-        return imgs, targets
-
     @torch.no_grad()
     def run(self, dev: torch.device):
         model = self.model.to(dev)
 
-        imgs, targets = self._load_images_targets()
+        imgs, targets = load_images_targets(self.data, self.num_images)
         imgs = torch.stack(imgs, 0).to(dev)
         targets = torch.stack(targets, 0)
         targets = F.one_hot(targets, num_classes=10).unsqueeze(-1).float().to(dev)
@@ -147,12 +183,56 @@ class InfillDigitsCommand:
         outer_parser.add_subcommand("infill", parser)
 
 
+class DensityEstimationCommand:
+    """Estimates marginal log p(X=x), assuming p(Y=y)=1/|Y|"""
+
+    def __init__(
+        self,
+        ckpt: str,
+        config: str,
+        num_images: int = 5,
+    ) -> None:
+        """
+        Args:
+            ckpt: Path to model parameters
+            config: Path to config.yaml to read datamodule configuration from
+            num_images: Number of images to generate
+        """
+        self.num_images = num_images
+        with open(config, "r") as f:
+            plcfg = yaml.safe_load(f.read())
+        self.data = datasets.MNISTDataModule(**plcfg["data"]["init_args"])
+        self.model = wave.WaveNet.load_from_checkpoint(ckpt).eval()
+
+    @torch.no_grad()
+    def run(self, dev: torch.device):
+        model = self.model.to(dev)
+
+        imgs, _ = load_images_targets(self.data, self.num_images)
+        imgs = torch.stack(imgs, 0).to(dev)
+        log_pxys = compute_log_pxy(imgs, model)
+        log_px = torch.logsumexp(log_pxys, -1)
+        print("p(x) x~p(MNIST)", log_px)
+
+        imgs_rand = torch.randint(0, 256, (5, 784)).to(dev)
+        log_pxys = compute_log_pxy(imgs_rand, model)
+        log_px = torch.logsumexp(log_pxys, -1)
+        print("p(x) x~U(0,255)", log_px)
+
+    @staticmethod
+    def add_arguments_to_parser(outer_parser):
+        parser = jsonargparse.ArgumentParser()
+        parser.add_class_arguments(DensityEstimationCommand, None)
+        outer_parser.add_subcommand("density", parser)
+
+
 @torch.no_grad()
 def main():
     parser = jsonargparse.ArgumentParser("WaveNet on MNIST")
     subcommands = parser.add_subcommands()
     SampleDigitsCommand.add_arguments_to_parser(subcommands)
     InfillDigitsCommand.add_arguments_to_parser(subcommands)
+    DensityEstimationCommand.add_arguments_to_parser(subcommands)
     config = parser.parse_args()
 
     dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -161,12 +241,14 @@ def main():
         cmd = SampleDigitsCommand(**(config.sample.as_dict()))
     elif config.subcommand == "infill":
         cmd = InfillDigitsCommand(**(config.infill.as_dict()))
+    elif config.subcommand == "density":
+        cmd = DensityEstimationCommand(**(config.density.as_dict()))
 
     cmd.run(dev)
 
 
 if __name__ == "__main__":
-    # python -m autoregressive.scripts.predict_mnist sample --ckpt "v58\checkpoints\wavenet-epoch=13-val_acc_epoch=0.8960.ckpt"
+    # python -m autoregressive.scripts.mnist sample --ckpt "v58\checkpoints\wavenet-epoch=13-val_acc_epoch=0.8960.ckpt"
 
-    # python -m autoregressive.scripts.predict_mnist infill --config v58\config.yaml --ckpt "v58\checkpoints\wavenet-epoch=13-val_acc_epoch=0.8960.ckpt"
+    # python -m autoregressive.scripts.mnist infill --config v58\config.yaml --ckpt "v58\checkpoints\wavenet-epoch=13-val_acc_epoch=0.8960.ckpt"
     main()
